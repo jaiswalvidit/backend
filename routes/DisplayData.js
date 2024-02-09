a23350ed8bac1634afc3f98dc894b8ac918e5b33
Vidit Jaiswal <jaiswalvidit23@gmail.com>
2024-02-09 13:10:10 +0530

    Dd

--- routes/DisplayData.js
+++ routes/DisplayData.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user'); // Import the User model
+router.get('/userdata/:email', async (req, res) => {
+    try {
+        const email = req.params.email; // Access email from params directly
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.json(user);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).json({ message: 'Error fetching user data', error: error.message });
+    }
+});
 
 
 
